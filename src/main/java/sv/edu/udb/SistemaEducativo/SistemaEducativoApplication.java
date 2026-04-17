e760312ea3bc82b4da06ccd26a42042e433be43d
Angel Argueta <angelargueta1807@gmail.com>
2026-04-17 11:00:12 -0800

    Agregado de dependencias

---
+++ src/main/java/sv/edu/udb/SistemaEducativo/SistemaEducativoApplication.java
@@ -0,0 +1,13 @@
+package sv.edu.udb.SistemaEducativo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaEducativoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaEducativoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sv/edu/udb/SistemaEducativo/SistemaEducativoApplicationTests.java
@@ -0,0 +1,13 @@
+package sv.edu.udb.SistemaEducativo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaEducativoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
